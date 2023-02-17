4a31ab44973b2b51a51349aa2376339717def06b
riswnda <62435376+riswnda@users.noreply.github.com>
2023-02-17 18:49:19 +0700

    update data intepreter v2

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/araddon/dateparse"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
 	"io"
 	"log"
@@ -41,9 +42,16 @@ func getData(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	ParsingFormat := data.ParsingFormat
-	targetData = checkFormat(ParsingFormat)
+	originFlatten := Flatten(data.OriginData)
+	targetData = checkFormat(ParsingFormat, originFlatten)
+	out, err := Unflatten(targetData)
+	spew.Dump(out)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	object, err := json.Marshal(targetData)
+	object, err := json.Marshal(out)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Printf("Error Marshal Response %s", err)
@@ -75,31 +83,68 @@ func Flatten(m map[string]any) map[string]any {
 	return o
 }
 
-func checkFormat(f []entity.Format) map[string]any {
+func Unflatten(flat map[string]any) (map[string]any, error) {
+	unflat := map[string]any{}
+
+	for key, value := range flat {
+		keyParts := strings.Split(key, ".")
+
+		// Walk the keys until we get to a leaf node.
+		m := unflat
+		for i, k := range keyParts[:len(keyParts)-1] {
+			v, exists := m[k]
+			if !exists {
+				newMap := map[string]any{}
+				m[k] = newMap
+				m = newMap
+				continue
+			}
+
+			innerMap, ok := v.(map[string]any)
+			if !ok {
+				return nil, fmt.Errorf("key=%v is not an object", strings.Join(keyParts[0:i+1], "."))
+			}
+			m = innerMap
+		}
+
+		leafKey := keyParts[len(keyParts)-1]
+		if _, exists := m[leafKey]; exists {
+			return nil, fmt.Errorf("key=%v already exists", key)
+		}
+		m[keyParts[len(keyParts)-1]] = value
+	}
+
+	return unflat, nil
+
+}
+
+func checkFormat(f []entity.Format, originFlatten map[string]any) map[string]any {
 	par := make(map[string]any)
-	originFlatten := Flatten(data.OriginData)
 
 	for _, parV := range f {
 		format, ft := getFormatType(parV.Format)
 		for key, value := range originFlatten {
+			//fmt.Printf("key dari originFlatten >>> %v\nparV.Origin            >>> %v\n\n", key, parV.Origin)
 			if strings.Contains(key, parV.Origin) {
 				switch ft {
 				case Empty:
 					par[parV.Target] = value
-				case String:
-					address, err := addressFormat(&format, key, fmt.Sprintf("%s", value))
-					if err == nil {
-						par[parV.Target] = address
-					}
 				case Date:
 					t, err := dateparse.ParseAny(fmt.Sprint(value))
 					if err != nil {
 						log.Fatal(err)
 					}
 					par[parV.Target] = t.Format(format)
+				case String:
+					address, err := addressFormat(&format, key, fmt.Sprintf("%s", value))
+					if err == nil {
+						par[parV.Target] = address
+					}
+					log.Fatal(err)
 
 				}
 			}
+
 		}
 	}
 	return par
@@ -107,8 +152,8 @@ func checkFormat(f []entity.Format) map[string]any {
 
 func getFormatType(f string) (format string, formatType FormatType) {
 	// Conditional to check empty format
-	if len(f) <= 0 {
-		return f, Empty
+	if len(f) == 0 {
+		return "", Empty
 	}
 	// Conditional to check date format
 	dateFormat, err := time.Parse(f, f)
